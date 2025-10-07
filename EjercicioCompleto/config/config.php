3c8aad0f19a832bb8ed720a43b998c03586947b1
DaniJimenz <danielji55@gmail.com>
2025-10-07 20:04:12 +0200

    Config

---
+++ EjercicioCompleto/config/config.php
@@ -0,0 +1,15 @@
+<?php
+
+define('SITE_NAME', 'Mi Sitio Web');
+define('SITE_URL', 'http://localhost');
+define('SITE_DESCRIPTION', 'Un sitio web modular con PHP');
+
+
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'mi_sitio');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+
+define('BASE_PATH', __DIR__ . '/../');
+define('INCLUDES_PATH', BASE_PATH . 'includes/');
+
