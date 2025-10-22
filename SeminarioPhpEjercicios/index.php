0b04a6f08ecee33789aa198bb606a77163debb1f
DaniJimz <danielji55@gmail.com>
2025-10-22 08:43:33 +0200

    Seminario ejercicios

--- SeminarioPhpEjercicios/index.php
+++ SeminarioPhpEjercicios/index.php
@@ -3,12 +3,13 @@
 //Ejercicio 1. Número máximo de un array
 //Crea una función que obtenga el número máximo de un array de números
 
-$array= [3, 5, 7, 2, 8, -1, 4];
+$array = [3, 5, 7, 2, 8, -1, 4];
+$numMax = $array[0];
 
-function($array) {
-    return max($array);
+
 }
 
 //Ejercicio 2. Sumatoria de un array
 //Crea una función que obtenga la sumatoria de un array de números
 
+?>
